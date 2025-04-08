d514a81a7096004c577d51ef991744b4e667f01f
Roberto Jimenez <robertojj2006@gmail.com>
2025-04-08 19:55:18 +0200

    test

--- gestor/tests.py
+++ gestor/tests.py
@@ -2,19 +2,38 @@ import copy
 import unittest
 import database as db
 
-
-class TestDatabase (unittest.TestCase):
+class TestDatabase(unittest.TestCase):
     def setUp(self):
-        db.clientes.lista_clientes = [
-            db.cliente("12345678A", "Juan", "Pérez"),
-            db.cliente("87654321B", "Ana", "Gómez"),
-            db.cliente("11223344C", "Luis", "Martínez")
+        # Se ejecuta antes de cada prueba
+        db.Clientes.lista = [
+            db.Cliente('15J', 'Marta', 'Pérez'),
+            db.Cliente('48H', 'Manolo', 'López'),
+            db.Cliente('28Z', 'Ana', 'García')
         ]
 
     def test_buscar_cliente(self):
-        cliente_existente= db.clientes.crear('39X', 'Héctor', 'Costa')
-        self .assertEqual(len(db.clientes.lista_clientes), 4)
+        cliente_existente = db.Clientes.buscar('15J')
+        cliente_no_existente = db.Clientes.buscar('99X')
+        self.assertIsNotNone(cliente_existente)
+        self.assertIsNone(cliente_no_existente)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('39X', 'Héctor', 'Costa')
+        self.assertEqual(len(db.Clientes.lista), 4)
         self.assertEqual(nuevo_cliente.dni, '39X')
         self.assertEqual(nuevo_cliente.nombre, 'Héctor')
         self.assertEqual(nuevo_cliente.apellido, 'Costa')
-        
+
+    def test_modificar_cliente(self):
+        cliente_a_modificar = copy.copy(db.Clientes.buscar('28Z'))
+        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana', 'Pérez')
+        self.assertEqual(cliente_a_modificar.nombre, 'Ana')
+        self.assertEqual(cliente_modificado.nombre, 'Mariana')
+
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.borrar('48H')
+        cliente_rebuscado = db.Clientes.buscar('48H')
+        self.assertNotEqual(cliente_borrado, cliente_rebuscado)
+
+if __name__ == '__main__':
+    unittest.main()
